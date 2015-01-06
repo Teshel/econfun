ce6576a9ab58c6850a5bef5c860131b6fb8d8120
Joseph Michaels <jmichaels@gmail.com>
2015-01-06 04:56:50 -1000

    basic generation functionality implemented

--- ngram.js
+++ ngram.js
@@ -2,7 +2,7 @@ var NGram = NGram || {};
 
 NGram.Corpus = function(data) {
 	this.words = data.split("\n");
-	this.
+	//this.
 	words.forEach(function(word) {
 
 	});
@@ -26,7 +26,7 @@ NGram.createModel = function(n, corpus) {
 	// Seeds stores the givens that are at the beginning of the word
 	// (i.e. those that start with "^")
 	var seeds = {};
-	var seedOffsets = {};
+	var seedOffsets = [];
 
 	// Monograms aren't implemented yet
 	if (n >= 2) {
@@ -43,13 +43,28 @@ NGram.createModel = function(n, corpus) {
 			}
 		});
 
+		var seedOffset = 0;
 		for (var givenKey in dist) {
 			var offset = 0;
 			distOffsets[givenKey] = [];
-			for (letterKey in dist[givenKey]) {
-				offset += dist[givenKey][letterKey];
-				distOffsets[givenKey].push([letterKey, offset]);
+
+			// if the given starts with a ^, this is a starting seed
+
+			//} else {
+				for (var letterKey in dist[givenKey]) {
+					offset += dist[givenKey][letterKey];
+					distOffsets[givenKey].push([letterKey, offset]);
+				}
+			//}
+			// 
+			if (givenKey.indexOf('^') === 0) {
+				seedOffset += offset
+				seedOffsets.push([givenKey, seedOffset]);
 			}
+				// for (var letterKey in dist[givenKey]) {
+				// 	seedOffset += dist[givenKey][letterKey];
+				// 	seedOffsets.push([givenKey+letterKey, seedOffset]);
+				// }
 		}
 	}
 	// might only need to return the offsets, not sure yet
@@ -76,22 +91,48 @@ NGram.App = function() {
 	}
 };
 
+NGram.randFromOffsetDist = function(offsets) {
+	var maxOffset = offsets[offsets.length-1][1];
+	var r = Math.floor(Math.random() * maxOffset);
+	
+	//for (var i=0; i<offsets.length; i++) {
+	var i = 0;
+	while (i < offsets.length && r >= offsets[i][1]) {
+		i++;
+	}
+	return offsets[i][0];
+}
+
 NGram.App.prototype = {
 	seedSequence: function(n) {
 		n = n || 2;
+		var model = this.models[n];
 
-
+		return NGram.randFromOffsetDist(model.seedOffsets);
 	},
 	// n refers to the n in n-gram
 	// minLength and maxLength are the minimum and maximum lengths
 	// of the word we want to generate
-	generate: function(n, minLength, maxLength) {
+	generate: function(n, maxLength) {
 		n = n || 2;
-		minLength = minLength || 4;
+		//minLength = minLength || 4;
 		maxLength = maxLength || 10;
 
 		// start with a seed
-		var seed = this.seedSequence(n);
+		var sequence = this.seedSequence(n);
+		var finish = false;
+		while (sequence.length < maxLength && !finish) {
+			var given = sequence.substring((sequence.length-(n-1)), sequence.length);
+			console.log(given);
+			var offsets = this.models[n].distOffsets[given];
+			if (offsets === undefined) {
+				finish = true;
+			} else {
+				sequence += NGram.randFromOffsetDist(offsets);
+			}
+			
+		}
+		return sequence;
 	}
 }
 
